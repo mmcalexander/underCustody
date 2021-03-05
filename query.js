865b240b4441216b4998b8a340a7307754fa40c6
mmcalexander <78703894+mmcalexander@users.noreply.github.com>
2021-03-04 21:23:34 -0500

    Create query.js

---
+++ query.js
@@ -0,0 +1,173 @@
+var jsonData = {
+  "Centres":[
+    {y: 60, label: "A.G.S. NORTE DE HUELVA"},
+    {y: 37, label: "A.G.S. CAMPO DE GIBRALTAR"},
+  {y: 31, label: "A.G.S. DE OSUNA"},
+  {y: 45, label: "A.G.S. ESTE DE MALAGA-AXARQUIA"},
+  {y: 84, label: "A.G.S. NORDESTE DE GRANADA"},
+  {y: 72, label: "A.G.S. NORTE DE ALMERIA"},
+  {y: 59, label:"A.G.S. NORTE DE CADIZ"},
+  {y: 43, label:"A.G.S. NORTE DE CORDOBA"},
+  {y: 126, label: "A.G.S. NORTE DE JAEN"},
+  {y: 30, label: "A.G.S. NORTE DE MALAGA"},
+  {y: 29, label: "A.G.S. SERRANIA DE MALAGA"},
+  {y: 46, label: "A.G.S. SUR DE CORDOBA"},
+  {y: 80, label: "A.G.S. SUR DE GRANADA"},
+  {y: 36, label: "A.G.S. SUR DE SEVILLA"},
+  {y: 40, label:"ALJARAFE"},
+  {y: 110, label:"ALMERIA"},
+  {y: 35, label: "BAHIA DE CADIZ - LA JANDA"},
+  {y: 23, label: "CONDADO - CAMPIÑA"},
+  {y: 23, label:"CORDOBA"},
+  {y: 24, label:"COSTA DEL SOL"},
+  {y: 24, label:"GRANADA"},
+  {y: 31, label:"GUADALQUIVIR"},
+  {y: 37, label:"HUELVA COSTA"},
+  {y: 38, label:"JAEN"},
+  {y: 31, label:"JAEN SUR"},
+  {y: 37, label:"MALAGA"},
+  {y: 142, label:"METROPOLITANO DE GRANADA"},
+  {y: 45, label:"PONIENTE DE ALMERIA"},
+  {y: 36, label:"SEVILLA"},
+  {y: 48, label:"SEVILLA NORTE"},
+  {y: 15, label:"VALLE DEL GUADALHORCE"}
+  ],
+
+    "By health centres":[
+      {y: 6, label: "A.G.S. NORTE DE HUELVA"},
+      {y: 11, label: "A.G.S. CAMPO DE GIBRALTAR"},
+    {y: 8, label: "A.G.S. DE OSUNA"},
+    {y: 9, label: "A.G.S. ESTE DE MALAGA-AXARQUIA"},
+    {y: 7, label: "A.G.S. NORDESTE DE GRANADA"},
+    {y: 7, label: "A.G.S. NORTE DE ALMERIA"},
+    {y: 20, label:"A.G.S. NORTE DE CADIZ"},
+    {y: 4, label:"A.G.S. NORTE DE CORDOBA"},
+    {y: 22, label: "A.G.S. NORTE DE JAEN"},
+    {y: 5, label: "A.G.S. NORTE DE MALAGA"},
+    {y: 4, label: "A.G.S. SERRANIA DE MALAGA"},
+    {y: 15, label: "A.G.S. SUR DE CORDOBA"},
+    {y: 9, label: "A.G.S. SUR DE GRANADA"},
+    {y: 21, label: "A.G.S. SUR DE SEVILLA"},
+    {y: 11, label:"ALJARAFE"},
+    {y: 20, label:"ALMERIA"},
+    {y: 23, label: "BAHIA DE CADIZ - LA JANDA"},
+    {y: 8, label: "CONDADO - CAMPIÑA"},
+    {y: 13, label:"CORDOBA"},
+    {y: 12, label:"COSTA DEL SOL"},
+    {y: 17, label:"GRANADA"},
+    {y: 7, label:"GUADALQUIVIR"},
+    {y: 14, label:"HUELVA COSTA"},
+    {y: 12, label:"JAEN"},
+    {y: 7, label:"JAEN SUR"},
+    {y: 27, label:"MALAGA"},
+    {y: 19, label:"METROPOLITANO DE GRANADA"},
+    {y: 14, label:"PONIENTE DE ALMERIA"},
+    {y: 32, label:"SEVILLA"},
+    {y: 15, label:"SEVILLA NORTE"},
+    {y: 7, label:"VALLE DEL GUADALHORCE"}
+    ],
+
+    "By doctor's office":
+    [
+      {y: 54, label: "A.G.S. NORTE DE HUELVA"},
+      {y: 26, label: "A.G.S. CAMPO DE GIBRALTAR"},
+    {y: 23, label: "A.G.S. DE OSUNA"},
+    {y: 36, label: "A.G.S. ESTE DE MALAGA-AXARQUIA"},
+    {y: 77, label: "A.G.S. NORDESTE DE GRANADA"},
+    {y: 65, label: "A.G.S. NORTE DE ALMERIA"},
+    {y: 39, label:"A.G.S. NORTE DE CADIZ"},
+    {y: 39, label:"A.G.S. NORTE DE CORDOBA"},
+    {y: 104, label: "A.G.S. NORTE DE JAEN"},
+    {y: 25, label: "A.G.S. NORTE DE MALAGA"},
+    {y: 25, label: "A.G.S. SERRANIA DE MALAGA"},
+    {y: 31, label: "A.G.S. SUR DE CORDOBA"},
+    {y: 71, label: "A.G.S. SUR DE GRANADA"},
+    {y: 15, label: "A.G.S. SUR DE SEVILLA"},
+    {y: 29, label:"ALJARAFE"},
+    {y: 90, label:"ALMERIA"},
+    {y: 12, label: "BAHIA DE CADIZ - LA JANDA"},
+    {y: 15, label: "CONDADO - CAMPIÑA"},
+    {y: 10, label:"CORDOBA"},
+    {y: 12, label:"COSTA DEL SOL"},
+    {y: 7, label:"GRANADA"},
+    {y: 24, label:"GUADALQUIVIR"},
+    {y: 23, label:"HUELVA COSTA"},
+    {y: 26, label:"JAEN"},
+    {y: 24, label:"JAEN SUR"},
+    {y: 10, label:"MALAGA"},
+    {y: 123, label:"METROPOLITANO DE GRANADA"},
+    {y: 31, label:"PONIENTE DE ALMERIA"},
+    {y: 4, label:"SEVILLA"},
+    {y: 33, label:"SEVILLA NORTE"},
+    {y: 8, label:"VALLE DEL GUADALHORCE"}
+    ],
+
+    "Ratio: population/centre":
+    [
+      {y: 1132.8, label: "A.G.S. NORTE DE HUELVA"},
+      {y: 6951.2, label: "A.G.S. CAMPO DE GIBRALTAR"},
+    {y: 5467.5, label: "A.G.S. DE OSUNA"},
+    {y: 3302.4, label: "A.G.S. ESTE DE MALAGA-AXARQUIA"},
+    {y: 1175.9, label: "A.G.S. NORDESTE DE GRANADA"},
+    {y: 1956.7, label: "A.G.S. NORTE DE ALMERIA"},
+    {y: 7522.7, label:"A.G.S. NORTE DE CADIZ"},
+    {y: 1774.5, label:"A.G.S. NORTE DE CORDOBA"},
+    {y: 2776.2, label: "A.G.S. NORTE DE JAEN"},
+    {y: 3649.3, label: "A.G.S. NORTE DE MALAGA"},
+    {y: 1825.2, label: "A.G.S. SERRANIA DE MALAGA"},
+    {y: 5656.5, label: "A.G.S. SUR DE CORDOBA"},
+    {y: 1735.0, label: "A.G.S. SUR DE GRANADA"},
+    {y: 11261.3, label: "A.G.S. SUR DE SEVILLA"},
+    {y: 9155.9, label:"ALJARAFE"},
+    {y: 2674.0, label:"ALMERIA"},
+    {y: 13951.9, label: "BAHIA DE CADIZ - LA JANDA"},
+    {y: 7004.5, label: "CONDADO - CAMPIÑA"},
+    {y: 13903.3, label:"CORDOBA"},
+    {y: 19999.5, label:"COSTA DEL SOL"},
+    {y: 11734.9, label:"GRANADA"},
+    {y: 3963.8, label:"GUADALQUIVIR"},
+    {y: 7730.8, label:"HUELVA COSTA"},
+    {y: 5170.6, label:"JAEN"},
+    {y: 2984.6, label:"JAEN SUR"},
+    {y: 16771.1, label:"MALAGA"},
+    {y: 2717.4, label:"METROPOLITANO DE GRANADA"},
+    {y: 5771.0, label:"PONIENTE DE ALMERIA"},
+    {y: 19327.6, label:"SEVILLA"},
+    {y: 5583.6, label:"SEVILLA NORTE"},
+    {y: 9062.7, label:"VALLE DEL GUADALHORCE"}
+    ]
+  }
+
+var dataPoints = [];
+var chart = new CanvasJS.Chart("chartContainer1",
+{
+ animationEnabled: true,
+	axisX: {
+    interval: 1
+  },
+  axisY2:{
+		interlacedColor: "rgba(1,77,101,.2)",
+		gridColor: "rgba(1,77,101,.1)",
+		title: ""
+  },
+
+	data: [{
+    type: 'bar',
+		color: "#014D65",
+    //xValueFormatString:"D MM h:mm",
+    name: "series1",
+    dataPoints: dataPoints // this should contain only specific serial number data
+
+	}]
+});
+
+$( ".dropdown" ).change(function() {
+	chart.options.data[0].dataPoints = [];
+  var e = document.getElementById("dd");
+	var selected = e.options[e.selectedIndex].value;
+  dps = jsonData[selected];
+  for(var i in dps) {
+    chart.options.data[0].dataPoints.push({label: dps[i].label, y: dps[i].y});
+  }
+  chart.render();
+})
